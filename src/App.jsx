837aff36c3e60215dab6292ce102925050790b54
Alex Faustino <faustino.alex@outlook.com>
2024-03-19 21:57:49 -0300

    feat: "add page style"

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Card } from './components/Card';
+
+function App() {
+  return (
+    <section>
+      <h1 className="text-main font-semibold text-center">Calculator.js</h1>
+      <Card />
+    </section>
+  );
+}
+
+export default App;
